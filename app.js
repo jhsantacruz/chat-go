1acbe952fc07c4f8ea549a916ad47d390bd5ffe9
Juan Santa Cruz <jhsantacruz@icloud.com>
2016-02-24 23:22:15 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let channels = [
+  {name: 'Hardware Support'},
+  {name: 'Software Support'}
+];
+
+class Channel extends React.Component {
+  onClick(){
+    console.log("clicked ->", this.props.name);
+  }
+  render(){
+    return (
+      <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+    )
+  }
+}
+
+class ChannelList extends React.Component{
+  render(){
+    return(
+      <ul>
+        {this.props.channels.map( channel => {
+          return (
+            <Channel name={channel.name} />
+          )
+        }
+        )}
+      </ul>
+    )
+  }
+}
+
+
+class ChannelForm extends React.Component{
+  render(){
+    return(
+      <form>
+        <input type="text" />
+      </form>
+    )
+  }
+}
+
+ReactDOM.render(<ChannelList channels={channels} />,
+  document.getElementById("app"));
